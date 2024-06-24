04ce4afd5f1cd573eaa2b742cfa8362813e684c1
giovannielbaruqui <giovanni.elbaruqui@gmail.com>
2024-06-24 18:46:01 -0300

    wip: agrupar pelo tipo de indicador

--- ExpectativasDeMercado/ViewModels/MainViewModel.cs
+++ ExpectativasDeMercado/ViewModels/MainViewModel.cs
@@ -10,6 +10,7 @@ public class MainViewModel : INotifyPropertyChanged
 {
     private readonly BacenApiService _bacenApiService;
     private string _selectedIndicador;
+    private string _selectedApiType;
     private DateTime _startDate;
     private DateTime _endDate;
     private ObservableCollection<object> _expectations; // Alterado para tipo genérico
@@ -19,7 +20,7 @@ public class MainViewModel : INotifyPropertyChanged
     {
         _bacenApiService = bacenApiService ?? throw new ArgumentNullException(nameof(bacenApiService));
         Expectations = new ObservableCollection<object>(); // Alterado para tipo genérico
-        Indicadores = new ObservableCollection<string> { "IPCA", "IGP-M", "Selic" };
+        ApiTypes = new ObservableCollection<string> { "Expectativa Mercado Mensais", "Expectativa Mercado" };
         LoadDataCommand = new RelayCommandAsync(async () => await LoadDataAsync(), () => !IsLoading);
         ClearDataCommand = new RelayCommandAsync(async () => await ClearData());
         ExportDataCommand = new RelayCommandAsync(async () => await ExportDataAsync());
@@ -28,12 +29,25 @@ public class MainViewModel : INotifyPropertyChanged
         OnPropertyChanged(nameof(DadosCarregados));
     }
 
-    public ObservableCollection<string> Indicadores { get; }
+    public ObservableCollection<string> ApiTypes { get; }
+
+    public ObservableCollection<string> Indicadores { get; private set; } = new ObservableCollection<string>();
 
     public string SelectedIndicador
     {
         get { return _selectedIndicador; }
         set { _selectedIndicador = value; OnPropertyChanged(); }
+    }
+
+    public string SelectedApiType
+    {
+        get { return _selectedApiType; }
+        set
+        {
+            _selectedApiType = value;
+            OnPropertyChanged();
+            UpdateIndicadores(); // Atualiza os indicadores quando o tipo de API é alterado
+        }
     }
 
     public DateTime StartDate
@@ -81,7 +95,7 @@ public class MainViewModel : INotifyPropertyChanged
         IsLoading = true;
         Expectations.Clear();
 
-        if (SelectedIndicador == "Selic")
+        if (SelectedApiType == "Expectativa Mercado")
         {
             var data = await _bacenApiService.GetSelicExpectations(StartDate, EndDate);
             foreach (var item in data)
@@ -105,6 +119,7 @@ public class MainViewModel : INotifyPropertyChanged
     private async Task ClearData()
     {
         SelectedIndicador = null;
+        SelectedApiType = null;
         StartDate = DateTime.Now.AddMonths(-1);
         EndDate = DateTime.Now;
         Expectations.Clear();
@@ -116,7 +131,7 @@ public class MainViewModel : INotifyPropertyChanged
         var csvLines = new List<string>();
 
         // Add headers based on the type of data
-        if (SelectedIndicador == "Selic")
+        if (SelectedApiType == "Expectativa Mercado")
         {
             csvLines.Add("Indicador,Data,Reuniao,Media,Mediana,DesvioPadrao,Minimo,Maximo,NumeroRespondentes,BaseCalculo");
             foreach (SelicExpectation item in Expectations)
@@ -148,5 +163,22 @@ public class MainViewModel : INotifyPropertyChanged
             string filePath = saveFileDialog.FileName;
             await File.WriteAllLinesAsync(filePath, csvLines);
         }
+    }
+
+    private void UpdateIndicadores()
+    {
+        Indicadores.Clear();
+
+        if (SelectedApiType == "Expectativa Mercado Mensais")
+        {
+            Indicadores.Add("IPCA");
+            Indicadores.Add("IGP-M");
+        }
+        else if (SelectedApiType == "Expectativa Mercado")
+        {
+            Indicadores.Add("Selic");
+        }
+
+        SelectedIndicador = Indicadores.FirstOrDefault();
     }
 }
